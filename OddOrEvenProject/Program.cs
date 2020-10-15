fff6006c705ca9aa5d8e7a6ea0b9aea5639d3a15
Elena Vonder Meulen <evonder13@gmail.com>
2020-10-15 15:43:06 -0400

    added comments/ Final Commit

--- OddOrEvenProject/Program.cs
+++ OddOrEvenProject/Program.cs
@@ -6,12 +6,13 @@ namespace OddOrEvenProject
     {
         static void Main(string[] args)
         {
+            //console.write lets them enter an answer
             Console.Write("Enter an Integer: ");
             var response = Console.ReadLine();
             //Console.WriteLine(response);
             var nbr = Convert.ToInt32(response);
             var isEven = true;
-            if (nbr % 2 == 1)//If nbr = 1 it's false
+            if (nbr % 2 == 1)//If nbr = 1 it's false(reciprical)
             { 
                 isEven = false;
             }
